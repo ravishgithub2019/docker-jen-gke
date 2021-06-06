a07a20e9f8a6112ef32324036ca584b393772104
ravishgithub2019 <49495377+ravishgithub2019@users.noreply.github.com>
2021-06-07 01:45:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Git-SSH-TEST_BUILD1! Docker! Jenkins! GKE! CICD1!'
+    return 'Hello, Git-SSH-TEST_BUILD1-UPDATE_WEBHOOK! Docker! Jenkins! GKE! CICD1!'
